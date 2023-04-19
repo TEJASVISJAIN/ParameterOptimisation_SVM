42f8cefa98d1a85ea91d4f07447d21cf0602f78c
Tejasvi S Jain <72309538+TEJASVISJAIN@users.noreply.github.com>
2023-04-20 02:48:34 +0530

    Update svm_iris.py

--- svm_iris.py
+++ svm_iris.py
@@ -1,73 +1,91 @@
-import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.svm import NuSVC
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import learning_curve
+from sklearn.datasets import make_classification
+import warnings
+warnings.filterwarnings('ignore')
+#dataset
 
-# Load the iris dataset from UCI
-iris_df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-iris_df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-iris_df['class'] = pd.factorize(iris_df['class'])[0]
-X = iris_df.iloc[:, :-1].values
-y = iris_df.iloc[:, -1].values
+X, y = make_classification(n_samples=np.random.randint(5000, 30001),
+                           n_features=10,
+                           n_informative=5,
+                           n_redundant=2,
+                           n_classes=4,
+                           n_clusters_per_class=2,
+                           random_state=42)
 
-# Divide the dataset into 70-30 for training and testing with 10 different samples
-n_samples = 10
-test_size = 0.3
-random_state = 42
-accuracies = []
+# convert to pandas DataFrame
+dataset = pd.DataFrame(np.c_[X, y], columns=[f'feature_{i}' for i in range(10)]+['class'])
 
-for i in range(n_samples):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    random_state += 1
+# create a numerical id for each class
+dataset['id'] = dataset.groupby(['class']).ngroup()
 
-    # Create the SVM model object
-    model = NuSVC(max_iter=1000)
+# drop the original class column
+dataset = dataset.drop(['class'],axis=1)
 
-    # Train the model on the training set
-    model.fit(X_train, y_train)
+# check for missing values
+dataset.isnull().sum()
 
-    # Predict the target values for the test set using the trained model
-    y_pred = model.predict(X_test)
+# drop rows with missing values
+dataset = dataset.dropna(axis=0,how='any')
 
-    # Evaluate the performance of the model
-    accuracy = accuracy_score(y_test, y_pred)
-    accuracies.append(accuracy)
+# scale the features
+X = dataset.iloc[:,0:-1]
+ss = StandardScaler()
+X = ss.fit_transform(X)
 
-    print(f"Sample {i + 1}: Accuracy = {accuracy}")
-    print("Best parameters: ", model.get_params())
-    print("-" * 80)
-
-# Find the sample with maximum accuracy
-max_accuracy_index = np.argmax(accuracies)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size,
-                                                    random_state=max_accuracy_index + random_state)
-model = NuSVC(kernel='rbf', nu=0.5, max_iter=1000)
-model.fit(X_train, y_train)
-
-# Plot the convergence graph for the sample whose accuracy is maximum
-plt.plot(model.n_support_, 'r-')
-plt.xlabel('Iterations')
-plt.ylabel('Number of support vectors')
-plt.title('Convergence graph for SVM')
-plt.show()
-
-# Plot the graph of accuracy vs number of iterations
-y_pred = model.predict(X_test)
-accuracies = []
-n_support = []
-for i in range(1, 1001):
-    model = NuSVC(kernel='rbf', nu=0.5, max_iter=i)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    n_support.append(model.n_support_.sum())
-    accuracies.append(accuracy)
-
-plt.plot(accuracies)
+# plot class distribution
+sns.countplot(x='id', data=dataset)
+samples = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=i)
+    samples.append((X_train, X_test, y_train, y_test))
+samples
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+result = pd.DataFrame(columns=['Sample', 'Best Accuracy', 'Best Kernel', 'Best Nu', 'Best Epsilon'])
+def fitnessFunction(kernel, C, gamma):
+  svm = SVC(kernel=kernel, C=C, gamma=gamma, max_iter=1000)
+  svm.fit(X_train, y_train)
+  y_pred = svm.predict(X_test)
+  return accuracy_score(y_pred, y_test)
+for i in range(len(samples)):
+  best_accuracy = 0
+  best_C = 0
+  best_gamma = 0
+  for kernel in kernels:
+    X_train, X_test, y_train, y_test = samples[i]
+    C = np.random.uniform(0, 10)
+    gamma = np.random.uniform(0, 10)
+    score = fitnessFunction(kernel, C, gamma)
+    if score>best_accuracy:
+      best_accuracy = round(score, 2)
+      best_C = round(C, 2)
+      best_gamma = round(gamma, 2)
+      best_kernel = kernel
+  print('Best Accuracy = ', best_accuracy, 'Best Kernel = ', best_kernel, 'Best Nu = ', best_C, 'Best Epsilon = ', best_gamma)
+  result.loc[i] = [i+1, best_accuracy, best_kernel, best_C, best_gamma]
+result
+X_train, X_test, y_train, y_test = samples[result['Best Accuracy'].idxmax()]
+train_sizes, train_scores, test_scores = learning_curve(SVC(kernel=result['Best Kernel'].iloc[result['Best Accuracy'].idxmax()], 
+                                                        C=result['Best Nu'].iloc[result['Best Accuracy'].idxmax()],
+                                                        gamma=result['Best Epsilon'].iloc[result['Best Accuracy'].idxmax()],
+                                                        max_iter = 1000), X_train, y_train, cv=10, scoring='accuracy', n_jobs=-1,
+                                                        train_sizes = np.linspace(0.01, 1.0, 50))
+train_sizes
+train_mean = np.mean(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+plt.figure(figsize=(10, 6))
+plt.plot(train_sizes, train_mean, label='Training Score')
+plt.plot(train_sizes, test_mean, label='Cross-Validation Score')
 plt.xlabel('Iterations')
 plt.ylabel('Accuracy')
-plt.title('Accuracy vs Number of Iterations')
+plt.title('Convergence Graph')
+plt.legend(loc="best")
 plt.show()
